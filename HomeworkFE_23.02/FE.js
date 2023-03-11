9371bc1e4081890346b2c6055cbcbbca162551f2
IuldashDavlietov <118936839+IuldashDavlietov@users.noreply.github.com>
2023-03-11 12:10:58 +0100

    Add files via upload

---
+++ HomeworkFE_23.02/FE.js
@@ -0,0 +1,55 @@
+// Task 1
+
+// let arr = [2, 4, 6, 5];
+// let mult = 1;
+// for( let i=0; i<arr.length; i++){
+//   mult*=arr[i]
+// } console.log(mult);
+
+// Task 2
+
+// let arr = [2, 4, 6, 5];
+// let sum = 1;
+// for(let i=0; i<arr.length; i++){
+//   sum+= sum + (arr[i] ** 2);
+// } console.log(sum);
+
+// Task 3
+
+// let arr = [2, 4, 6, 5];
+// let max = arr[0];
+// for(let i=0; i<arr.length; i++){
+// if (arr[i] > max){
+// max = arr[i]
+// }
+// } console.log(max);
+
+// Task 4
+
+// let arr = [7, 2, 4, 6, 5];
+// let min = arr[0];
+// for(let i=0; i<arr.length; i++){
+// if (arr[i] < min){
+// min = arr[i]
+// }
+// } console.log(min);
+
+// task 5
+
+// let arr = [7, 2, 4, 6, 5];
+// let sum = 0;
+// avg = 0;
+// for( let i=0; i<arr.length; i++){
+//   sum+= arr[i];
+//   avg = sum/arr.length;
+// } console.log(avg);
+
+// Task 6
+
+// let arr = [5, 7, 2, -10, 1, -3];
+// for( let i=0; i<arr.length;i++){
+//   if( arr[i] > 0){
+//     console.log(arr.length)
+//   }
+// }
+
